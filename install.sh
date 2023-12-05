f3365776d710693972aa39c70d7ce96a834bd260
liangzid <2273067585@qq.com>
2023-12-05 19:32:21 +0800

    update install scripts.

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 sudo cp *.ttf /usr/share/fonts/
+sudo cp *.ttc /usr/share/fonts/
+git clone https://github.com/liangzid/fonts-from-mps
+cd fonts-from-mps
+sudo cp *.ttf /usr/share/fonts/
+sudo cp *.TTF /usr/share/fonts/
 sudo fc-cache -fv
 echo "added done."
